a646b02900f21a169944e0fc7568b6fea6466853
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-12-24 03:28:27 +0900

    Added sending web api request.

--- Resources/input.js
+++ Resources/input.js
@@ -8,7 +8,15 @@ function loadInput() {
                                            width: '200%',
                                            top: 170});
 
-    buttion.addEventListener('click', function(e) {
+    button.addEventListener('click', function(e) {
+            var xhr = Titanium.Network.createHTTPClient();
+            xhr.onload = function() {
+                var mess = JSON.parse(this.responseText);
+                var diag = Titanium.UI.createAlertDialog({message: mess.result});
+                diag.show();
+            };
+            xhr.open("POST", "http://192.168.11.8:5000/input");
+            xhr.send({word: text.value});
         });
     var controls = Ti.UI.createView({
             width: 270,
